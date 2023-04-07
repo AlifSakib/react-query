a81add50f1e0b958861cc5593ddcd3e195ecf2f0
AlifSakib <alifsakib@gmail.com>
2023-04-07 14:04:35 +0600

    added query refetching

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import "./App.css";
 
 const POSTS = [
@@ -6,11 +6,27 @@ const POSTS = [
   { id: 2, title: "Post 2" },
 ];
 
+// /posts -> ["posts"]
+// /posts/1 -> ["posts", 1]
+// /posts/1/comments -> ["posts", 1, "comments"]
+// posts?authorId=1 -> ["posts", { authorId: 1 }]
+
 function App() {
+  const queryClient = useQueryClient();
   const postQuery = useQuery({
     queryKey: ["posts"],
     // queryFn: () => Promise.reject("Error Message"),
     queryFn: () => wait(1000).then(() => [...POSTS]),
+  });
+
+  const newPostMutation = useMutation({
+    mutationFn: async (title) => {
+      await wait(1000);
+      return POSTS.push({ id: crypto.randomUUID(), title });
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(["posts"]);
+    },
   });
 
   if (postQuery.isLoading) return <div>Loading...</div>;
@@ -19,10 +35,17 @@ function App() {
     <div>
       {postQuery.data.map((post) => (
         <>
-          <div key={post.id}></div>
-          <h3>{post.title}</h3>
+          <div key={post.id}>
+            <h3>{post.title}</h3>
+          </div>
         </>
       ))}
+      <button
+        disabled={newPostMutation.isLoading}
+        onClick={() => newPostMutation.mutate("New Post")}
+      >
+        Add New
+      </button>
     </div>
   );
 }
